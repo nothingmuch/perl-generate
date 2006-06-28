0420b7ba1f83a64610f5dc528b506914c32c862b
Yuval Kogman <nothingmuch@woobling.org>
2006-06-28 21:47:19 +0000

    Node::Expr - base role for expressions

---
+++ lib/Perl/Generate/AST/Node/Expr.pm
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+package Perl::Generate::AST::Node::Expr;
+use Moose::Role;
+
+with qw/
+	Perl::Generate::AST::Node
+/;
+
+__PACKAGE__;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Perl::Generate::AST::Node::Expr - Perl 5 AST node for expressions.
+
+=cut

6b9e3579c9d97d58e1ae7797909ed8893e7ca1bc
Yuval Kogman <nothingmuch@woobling.org>
2006-06-28 21:38:24 +0000

    Add the Perl::Generate::AST::Node base role

---
+++ lib/Perl/Generate/AST/Node.pm
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+package Perl::Generate::AST::Node;
+use Moose::Role;
+
+use PPI ();
+
+requires "ppi";
+
+sub stringify {
+	my $self = shift;
+
+	my $f = PPI::Document::Fragment->new;
+	$f->add_element( $self->ppi );
+	return $f->serialize;
+}
+
+__PACKAGE__;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Perl::Generate::AST::Node - Base role for Perl 5 AST nodes.
+
+=head1 SYNOPSIS
+
+	use Moose;
+
+	with qw/Perl::Generate::AST::Node/;
+
+=head1 DESCRIPTION
+
+All nodes in L<Perl::Generate::AST> do this role.
+
+=cut
+
+
